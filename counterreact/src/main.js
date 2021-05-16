280d87753c1280b11c0827c0b820f67dfb10983a
Arun Regmi <aregmi450@gmail.com>
2021-05-16 15:58:05 +0545

    counter in reaCT

---
+++ counterreact/src/main.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div style={{marginTop:'200px'}}>
+
+                <h1>React Counter</h1>
+                <h1>0</h1>
+                <button className='btn btn-success'>Increment</button>
+                <button className='btn btn-danger'>Decrement</button>
+                <button className='btn btn-primary'>Neutral</button>
+            </div>
+         );
+    }
+}
+ 
+export default Main;
+
+// class component only have render option function doesn't have it
