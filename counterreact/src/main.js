56d06afedf2b681d32b2bc833d425afa1652a711
Arun Regmi <aregmi450@gmail.com>
2021-05-16 17:18:14 +0545

    second project complete

--- counterreact/src/main.js
+++ counterreact/src/main.js
@@ -3,18 +3,36 @@ import React, {Component} from 'react'
 class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            number: 0
+         }
+    }
+    
+    //  defining functions to use 
+    increment = () => {
+        this.setState({number: this.state.number+1})
+    }
+    decrement = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+    neutral = () => {
+        this.setState({ number: 0 })
     }
     render() { 
         return ( 
-            <div style={{marginTop:'200px'}}>
+            <div className='row justify-content-center'>
+                <div className='col-md-5' style={{ marginTop: '200px', border: '5px solid black' }}>
 
-                <h1>React Counter</h1>
-                <h1>0</h1>
-                <button className='btn btn-success'>Increment</button>
-                <button className='btn btn-danger'>Decrement</button>
-                <button className='btn btn-primary'>Neutral</button>
+                    <h1>React Counter</h1>
+                    {/* to use external variable in html content wrtie in curly braces  */}
+                    <h1>{this.state.number}</h1>
+                    <button className='btn btn-success' onClick={this.increment}>Increment</button>
+                    <button className='btn btn-danger' onClick={this.decrement}>Decrement</button>
+                    <button className='btn btn-primary' onClick={this.neutral}>Neutral</button>
+                </div>
+
             </div>
+           
          );
     }
 }
