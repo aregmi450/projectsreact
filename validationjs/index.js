930fa1f5bfd9ba54709620ec08a42bba2148b351
Arun Regmi <aregmi450@gmail.com>
2021-05-20 16:08:39 +0545

    fifth project completed

---
+++ validationjs/index.js
@@ -0,0 +1,72 @@
+function validate()
+{
+    //access input values 
+
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var cpassword = document.getElementById('password').value
+
+//print values in console to see if we are getting it or not
+// console.log(username + email);
+
+
+    //check username and verify username condition 
+    checkusername(username)
+    checkemail(email)
+    checkpassword(password)
+    checkpasswordsmatch(password, cpassword)
+}
+
+function checkusername(username)
+{
+    if(username.length > 7)
+    {
+        document.getElementById('username').classList.add('success')
+        document.getElementById('username').classList.replace('error', 'success')
+        document.getElementById('username_error').innerHTML=''
+    }
+    else{
+        document.getElementById('username').classList.add('error')
+        document.getElementById('username_error').innerText='username must be 8 letters long'
+    }
+}
+
+function checkemail(email) {
+    if (email.length > 8 && email.includes('@gmail')) {
+        document.getElementById('email').classList.add('success')
+        document.getElementById('email').classList.replace('error', 'success')
+        document.getElementById('email_error').innerHTML = ''
+    }
+    else {
+        document.getElementById('email').classList.add('error')
+        document.getElementById('email_error').innerText = 'Enter valid email '
+    }
+}
+
+function checkpassword(password) {
+    if (password.length > 6) {
+        document.getElementById('password').classList.add('success')
+        document.getElementById('password').classList.replace('error', 'success')
+        document.getElementById('password_error').innerHTML = ''
+    }
+    else {
+        document.getElementById('password').classList.add('error')
+        document.getElementById('password_error').innerText = 'Your password must be 6 digits long'
+    }
+}
+
+function checkpasswordsmatch(password, cpassword)
+{
+    if(password==cpassword && password!='')
+    {
+        document.getElementById('cpassword').classList.add('success')
+        document.getElementById('cpassword').classList.replace('error', 'success')
+        document.getElementById('cpassword_error').innerHTML = ''
+    }
+    else{
+        document.getElementById('cpassword').classList.add('error')
+        document.getElementById('cpassword_error').innerText = 'Passwords not matched'
+    }
+}
+
