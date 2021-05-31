98ee3dac621d785494b59485099d4356f859ec1e
Arun Regmi <aregmi450@gmail.com>
2021-05-31 23:47:53 +0545

    eighth project completed

--- covidjs/index.js
+++ covidjs/index.js
@@ -10,7 +10,8 @@
         for(var i=0; i<response.Countries.length; i++)
         {
             var totalactive = response.Countries[i].TotalConfirmed - response.Countries[i].TotalRecovered
-            var tablerow = `<tr> <td>${response.Countries[i].Country}</td> <td>${response.Countries[i].TotalConfirmed}</td> <td>${totalactive}</td> <td>${response.Countries[i].TotalRecovered}</td> <td>${response.Countries[i].TotalDeaths}</td>    </tr>`
+            // var recoverrate = response.Countires[i].TotalRecovered / response.Countries[i].TotalConfirmed
+            var tablerow = `<tr> <td>${response.Countries[i].Country}</td> <td>${response.Countries[i].TotalConfirmed}</td> <td>${totalactive}</td> <td>${response.Countries[i].TotalRecovered}</td> <td>${response.Countries[i].TotalDeaths}</td> </tr>`
 
             $('#tbody').append(tablerow)
         }
